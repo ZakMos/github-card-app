8e242a2708247e53dc4b96c41f8d6757be83aa20
Zakaria Moslim <z.zza@hotmail.com>
2019-03-07 15:34:46 +0100

    added Card.js in Components folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import Card from './components/Card';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <p>Hello World</p>
+        <Card />
       </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Card extends Component {
+  render() {
+    return (
+      <div ClassName= "github-profile">
+      One GitHub Profile...
+      </div>
+    );
+  }
+}
+
+export default Card;
