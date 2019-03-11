65235c9b0fa637aad0d6bfecf14b5cd1db25cabb
Zakaria Moslim <z.zza@hotmail.com>
2019-03-11 13:03:44 +0100

    add alt info for img - 2

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ class Card extends Component {
   render() {
     return (
       <div className= "github-profile">
-        <img src="https://placehold.it/75" />
+        <img src="https://placehold.it/75" alt="user-info"/>
           <div className="info">
             <div className="name">Name Here...</div>
             <div className="company">Company Here...</div>
