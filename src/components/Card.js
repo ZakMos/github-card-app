75cab48f87d59bdce2d53e2ee4342da75b0dc236
Zakaria Moslim <z.zza@hotmail.com>
2019-03-11 13:40:36 +0100

    add some comments, and change the manually adding data to auto using maps

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 
 
 
+
 class Card extends Component {
   render() {
+    // في هذه الحالة يتم استدعاء طلب واحد فقط، ولتغيير هذا، يمكن استبداله للكود التالي:
+    // const profile = testData[0];
+
+    // هنا تم تغييره لبروبس.. وتم إضافة الكود الاول تيست داتا إلى ملف كارد ليست ـ ليستدعي البروبس من هنا، ونقل كامل التيست داتا إلى ملف الكارد ليست
+    const profile = this.props;
+
     return (
       <div className= "github-profile">
-        <img src="https://placehold.it/75" alt="user-info"/>
+        <img src={profile.avatar_url} alt="user-info"/>
           <div className="info">
-            <div className="name">Name Here...</div>
-            <div className="company">Company Here...</div>
+            <div className="name">{profile.name}</div>
+            <div className="company">{profile.company}</div>
           </div>
-      One GitHub Profile...
       </div>
     );
   }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Card from './Card';
-
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "Facbook"},
+  {name: "Sophi Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Facbook"},
+  {name: "Sebastian Markbage", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facbook"},
+];
 const CardList = (props) =>(
       <div>
-        <Card />
+        {testData.map(profile => <Card {...profile} />)}
       </div>
 );
 
