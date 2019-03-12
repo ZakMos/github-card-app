96341ba1f6716e0a5f5f8a63869b1fc7587e8b76
Zakaria Moslim <z.zza@hotmail.com>
2019-03-12 12:16:33 +0100

    add form with event

---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  state = { userName: ''}
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.userName)
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <input
+        type="text"
+        value={this.state.userName}
+        onChange= {event => this.setState ({ userName: event.target.value })}
+        placeholder="Github Username"
+        required
+         />
+        <button>Add Card</button>
+        </form>
+    );
+  }
+}
+
+
+export default Form;
