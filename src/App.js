12553845363bdff426b61b0ab917ac81128ac415
Zakaria Moslim <z.zza@hotmail.com>
2019-03-12 12:16:01 +0100

    add constructor and import testData to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import CardList from './components/CardList';
+import Form from './components/Form';
 import './App.css';
 
+
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "Facbook"},
+  {name: "Sophi Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Facbook"},
+  {name: "Sebastian Markbage", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facbook"},
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      profiles: testData,
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <p>Hello World</p>
-        <CardList />
+        <Form />
+        <CardList profiles={this.state.profiles} />
       </div>
     );
   }
